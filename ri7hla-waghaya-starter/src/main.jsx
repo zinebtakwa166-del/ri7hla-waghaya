9c9f8f9166426fe9448e390cdf5a9e90f190cd0e
zinebtakwa166-del <zinebtakwa166@gmail.com>
2025-12-05 14:10:17 +0100

    Update main.jsx

--- ri7hla-waghaya-starter/src/main.jsx
+++ ri7hla-waghaya-starter/src/main.jsx
@@ -1,4 +1,3 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -8,5 +7,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
 
 
