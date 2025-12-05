f6270e11cdc23feb6834ca33e64d020ec5784f57
zinebtakwa166-del <zinebtakwa166@gmail.com>
2025-12-05 12:42:08 +0100

    Update App.jsx

--- ri7hla-waghaya-starter/src/App.jsx
+++ ri7hla-waghaya-starter/src/App.jsx
@@ -1,4 +1,11 @@
+// ri7hla-waghaya-starter/src/App.jsx
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Catalog from './pages/Catalog.jsx';
+import CreateTrip from './pages/CreateTrip.jsx';
+
+const Nav = () => (
+  <nav style={{display:'flex',
 
 
